1451e1915768f2089646403908ebd9a50d9b2589
stawitcc <c.c.stawitz@bham.ac.uk>
2025-10-17 13:52:01 +0100

    first draft of WQ plots

---
+++ R/fct_make_plot_data.R
@@ -0,0 +1,268 @@
+#' make_recent_inv_spp
+#'
+#' @description This function makes the data for the recent invasive species plot
+#' @param cleaned_data The cleaned invasive species data
+#' @param sampling_locs The sampling locations for invasive species data
+#' @param plot_palette The colour palette to use for the plot
+#' @return The data frame for the recent invasive species plot.
+#' @importFrom dplyr left_join select rename arrange filter mutate across group_by slice_head ungroup
+#' @importFrom tidyr pivot_longer drop_na
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom lubridate dmy years
+#' @noRd
+make_recent_inv_spp <- function(cleaned_data, sampling_locs, plot_palette) {
+    BRCInvSpcs_Plot <-
+        dplyr::left_join(
+            # Select lat and long from the locations data frame and add to the invasive species date
+            cleaned_data, # only the first lat and long are used per location
+            sampling_locs[, c(
+                "sampling_site",
+                "LAT",
+                "LONG"
+            )],
+            multiple = "first"
+        ) |>
+        dplyr::select(
+            -id,
+            -invasive_spp_what_three_words,
+            -any_other_invasive_spp,
+            -data_type
+        )
+
+    BRCInvSpcs_Plot <- BRCInvSpcs_Plot |> # Remove unneeded columns
+        tidyr::pivot_longer(
+            -c(
+                organisation,
+                survey_date,
+                invasive_spp_sampling_date,
+                sampling_site,
+                LONG,
+                LAT
+            ),
+            names_to = "variable",
+            values_to = "value"
+        ) |>
+        dplyr::filter(value != "") |>
+        dplyr::rename(c(
+            'measurement' = variable,
+            'abundance' = value
+        )) |>
+        mutate(
+            InvSpcs_Plot_Colour = dplyr::case_when(
+                abundance == ">1000" ~ plot_palette[1],
+                abundance == "100-999" ~ plot_palette[2],
+                abundance == "10-99" ~ plot_palette[3],
+                abundance == "1-9" ~ plot_palette[4],
+                abundance == "Abundant (>33%)" ~ plot_palette[1],
+                abundance == "Present (1-33%)" ~ plot_palette[3]
+            )
+        ) |>
+        mutate(date_time = lubridate::dmy(invasive_spp_sampling_date)) |>
+        dplyr::arrange(sampling_site, date_time) |>
+        dplyr::select(-survey_date, -invasive_spp_sampling_date) |>
+        dplyr::mutate(sampling_site = gsub("\\s+", " ", sampling_site)) |> # Remove the parenthesised organisation from the site ID
+        mutate(across(sampling_site, flip_site_names))
+
+    # Anonymise those that want to be based on the sign up sheet
+    BRCInvScs_Plot <- anonymise_organisations(BRCInvSpcs_Plot)
+
+    BRCInvSpcs_Plot_Recent <- BRCInvSpcs_Plot |>
+        filter(date_time >= Sys.Date() - lubridate::years(3))
+    BRCInvSpcs_Plot_Recent$abundance <- factor(
+        BRCInvSpcs_Plot_Recent$abundance,
+        levels = c(
+            ">1000",
+            "100-999",
+            "10-99",
+            "1-9",
+            "Abundant (>33%)",
+            "Present (1-33%)"
+        )
+    )
+
+    # This selects the highest observation per taxa per site based on the factor levels set above
+    BRCInvSpcs_Plot_Recent <- BRCInvSpcs_Plot_Recent |>
+        dplyr::arrange(
+            sampling_site,
+            measurement,
+            abundance,
+            desc(date_time)
+        ) |>
+        dplyr::group_by(sampling_site, measurement) |>
+        dplyr::slice_head(n = 1) |>
+        dplyr::ungroup()
+
+    return(BRCInvSpcs_Plot_Recent)
+}
+
+#' species_plots
+#'
+#' Function to create the data for the Riverfly plots
+#' @param sampling_locs The sampling locations for Riverfly data
+#' @return a list with four elements: Riverfly_Species_Plot which
+#' has all Riverfly species, Riverfly_Species_Plot_Recent which has
+#' all observations of Riverfly species for the past year,
+#' Other_Species_Plot which has all observations of other species, and
+#' Other_Species_Plot_Recent
+#' @importFrom dplyr select rename group_by mutate ungroup case_when
+#' @importFrom tidyr pivot_longer
+species_plots <- function(riverfly_data, sampling_locs) {
+    # Join the riverfly data with the locations data to get lat and long
+    Riverfly_Species_Plot <- left_join(
+        riverfly_data,
+        sampling_locs[, c(
+            "sampling_site",
+            "LAT",
+            "LONG"
+        )],
+        by = join_by(sampling_site),
+        multiple = "first"
+    ) |>
+        dplyr::select(organisation, everything())
+
+    # Anonymise those that want to be based on the sign up sheet
+    # flip names, and remove parenthesised orgs
+    Riverfly_Species_Plot <- Riverfly_Species_Plot |>
+        anonymise_organisations() |>
+        remove_parenthesised_orgs() |>
+        mutate(across(sampling_site, flip_site_names))
+
+    # Create 2 separate sets - One of other species and a table (like invasive species) - do this first for now
+    Riverfly_Other_Species_Plot <- Riverfly_Species_Plot |>
+        dplyr::select(
+            survey_date,
+            sampling_site,
+            organisation,
+            LAT,
+            LONG,
+            one_of(names(other_spp_bw))
+        )
+    # Then the other set for Urban Riverfly taxa and a ggplot (like ARMI) - did this second here so I can overwrite df name
+    Riverfly_Species_Plot <- Riverfly_Species_Plot |>
+        dplyr::select(
+            survey_date,
+            sampling_site,
+            organisation,
+            LAT,
+            LONG,
+            one_of(names(riverfly_spp_bw))
+        )
+
+    # Converted to numeric as it represents y-axis (labels edited in ggplot code in server below)
+    Riverfly_Species_Plot <- Riverfly_Species_Plot |>
+        pivot_longer(
+            -c(
+                organisation,
+                survey_date,
+                sampling_site,
+                LONG,
+                LAT
+            )
+        ) |>
+        dplyr::rename(taxa = "name", abundance = "value") |>
+        group_by(sampling_site, taxa) |>
+        filter(!all(abundance == "0")) |>
+        ungroup() |>
+        mutate(
+            abundance = as.numeric(case_when(
+                abundance == "0" ~ 0,
+                abundance == "1-9" ~ 1,
+                abundance == "10-99" ~ 2,
+                abundance == "100-999" ~ 3,
+                abundance == ">1000" ~ 4
+            ))
+        ) |>
+        mutate(survey_date = dmy(survey_date))
+
+    # Then find the highest/most recent - factor fine for the purpose of the leaflet marker
+    Riverfly_Species_Plot_Recent <- Riverfly_Species_Plot |>
+        filter(survey_date >= Sys.Date() - years(3)) |>
+        arrange(sampling_site, taxa, desc(abundance), desc(survey_date)) |>
+        group_by(sampling_site, taxa) |>
+        slice_head(n = 1) |>
+        ungroup() |>
+        mutate(
+            Riverfly_Species_Colour = factor(
+                case_when(
+                    abundance == 0 ~ brewer.pal(n = 5, name = "Greys")[1],
+                    abundance == 1 ~ brewer.pal(n = 5, name = "Greys")[2],
+                    abundance == 2 ~ brewer.pal(n = 5, name = "Greys")[3],
+                    abundance == 3 ~ brewer.pal(n = 5, name = "Greys")[4],
+                    abundance == 4 ~ brewer.pal(n = 5, name = "Greys")[5]
+                ),
+                levels = brewer.pal(n = 5, name = "Greys")
+            )
+        )
+
+    # Organise - this plot df not used in the end. Seemed off trying to plot inconsistently ID'd taxa. So did a pop up table of the most recent instead (like invasive)
+    Riverfly_Other_Species_Plot <- Riverfly_Other_Species_Plot |>
+        pivot_longer(
+            -c(organisation, sampling_site, survey_date, LONG, LAT)
+        ) |>
+        dplyr::rename(taxa = "name", abundance = "value") |>
+        group_by(sampling_site, taxa) |>
+        filter(!all(abundance == "0")) |>
+        ungroup() |>
+        mutate(
+            abundance = factor(
+                abundance,
+                levels = c(">1000", "100-999", "10-99", "1-9")
+            )
+        ) |>
+        mutate(survey_date = dmy(survey_date))
+
+    # Then find the highest/most recent
+    Riverfly_Other_Species_Plot_Recent <- Riverfly_Other_Species_Plot |>
+        filter(survey_date >= Sys.Date() - years(3)) |>
+        arrange(sampling_site, taxa, abundance, desc(survey_date)) |>
+        group_by(sampling_site, taxa) |>
+        slice_head(n = 1) |>
+        ungroup()
+
+    return(list(
+        Riverfly_Species_Plot,
+        Riverfly_Species_Plot_Recent,
+        Riverfly_Other_Species_Plot,
+        Riverfly_Other_Species_Plot_Recent
+    ))
+}
+
+#' make_water_quality_plot_data
+#'
+#' Function to create the data for the Water Quality plots
+#' @param water_quality_data The cleaned water quality data
+#' @param sampling_locs The sampling locations for water quality data
+#' @param reading_type The type of water quality reading (e.g., "Temperature (°C)")
+#' @return A data frame for the water quality plot
+#' @importFrom dplyr left_join select mutate across
+#' @importFrom RColorBrewer brewer.pal
+make_water_quality_plot_data <- function(
+    water_quality_data,
+    sampling_locs,
+    reading_type
+) {
+    # Placeholder function for future water quality plot data processing
+    water_quality_plots <- left_join(
+        water_quality_data,
+        sampling_locs[, c(
+            "sampling_site",
+            "LAT",
+            "LONG"
+        )],
+        by = join_by(sampling_site),
+        multiple = "first"
+    ) |>
+        dplyr::select(organisation, everything()) |>
+        anonymise_organisations() |>
+        remove_parenthesised_orgs() |>
+        mutate(across(sampling_site, flip_site_names)) |>
+        mutate(
+            WQ_Plot_Colour = cut(
+                value,
+                breaks = c(-Inf, 5:12, Inf),
+                labels = brewer.pal(n = 9, name = "Blues")
+            )
+        )
+
+    return(water_quality_plot)
+}
--- R/mod_03_fct_leaflet_plots.R
+++ R/mod_03_fct_leaflet_plots.R
@@ -643,3 +643,168 @@ addInvasiveSpeciesMarkers <- function(
             )
     }
 }
+
+addWaterQualityMarkers <- function(
+    mapProxy,
+    wq_data,
+    reading_type
+) {
+    mapProxy |> clearPopups() |> clearGroup("points")
+
+    reading_domain <- levels(data$reading)
+
+    if (!is.null(data) && nrow(data) > 0) {
+        plotPopups <- function(i, popup_width) {
+            site_id <- data$sampling_site[i]
+            organisation <- data$organisation[i]
+
+            wqdata_SiteID <- filter(
+                wq_data,
+                sampling_site == site_id
+            )
+            ##Some organisations don't sound right with "the" in front
+            organisation <- if (
+                organisation != "Hall Green's Keepin' It Clean" &
+                    organisation != "Birmingham Conservation Society"
+            ) {
+                organisation <- paste("the", organisation)
+            } else {
+                organisation <- organisation # This line is optional, just for clarity
+            }
+
+            # Calculate date range buffer if there's only one sample
+            date_range <- range(
+                wqdata_SiteID$survey_date,
+                na.rm = TRUE
+            )
+            if (diff(date_range) == 0) {
+                date_range <- c(date_range[1] - 15, date_range[2] + 15)
+            }
+
+            # Set character width for str_wrap based on popup width
+            title_wrap_width <- ifelse(
+                popup_width <= 300,
+                37,
+                ifelse(popup_width <= 450, 50, 75)
+            )
+            title_text <- paste0(
+                "Water quality at ",
+                site_id,
+                ". Sampled by ",
+                organisation,
+                "."
+            )
+
+            p <- ggplot(
+                wqdata_SiteID,
+                aes(
+                    x = as.Date(survey_date),
+                    y = value,
+                    fill = cut(
+                        value,
+                        breaks = c(-Inf, 5:12, Inf),
+                        labels = c(
+                            "≤5",
+                            "6",
+                            "7",
+                            "8",
+                            "9",
+                            "10",
+                            "11",
+                            "12",
+                            "≥13"
+                        )
+                    )
+                )
+            ) +
+                geom_point(size = 5, pch = 21, colour = "black") +
+                theme_minimal() +
+                scale_fill_manual(
+                    name = reading_type,
+                    values = brewer.pal(n = 9, name = "Blues"),
+                    drop = FALSE
+                ) +
+                xlab("Survey Date") +
+                ylab(reading_type) +
+                scale_x_date(
+                    date_breaks = "1 month",
+                    date_labels = "%b '%y",
+                    limits = date_range
+                ) +
+                theme(
+                    plot.title.position = "plot",
+                    axis.title.x = element_text(face = "bold"),
+                    axis.title.y = element_text(face = "bold"),
+                    legend.position = "none",
+                    plot.title = element_text(
+                        size = 13,
+                        face = "bold",
+                        hjust = 0.5
+                    )
+                ) + ##Did have the text over the y-axis title, but changed to centre - ","
+                ggtitle(str_wrap(title_text, width = title_wrap_width)) # Wrap the title based on width
+
+            return(p)
+        }
+
+        # Adjust plot size based on screen width
+        observe({
+            if (!is.null(input$screen_width)) {
+                if (input$screen_width <= 480) {
+                    # For small screens like iPhones
+                    popup_width <- 300
+                    popup_height <- 250
+                } else if (input$screen_width <= 768) {
+                    # For tablets
+                    popup_width <- 400
+                    popup_height <- 275
+                } else {
+                    # For larger screens
+                    popup_width <- 600
+                    popup_height <- 350
+                }
+            } else {
+                popup_width <- 600
+                popup_height <- 350
+            }
+
+            plots <- lapply(1:nrow(data), function(i) {
+                plotPopups(i, popup_width)
+            })
+
+            mapProxy |>
+                addCircleMarkers(
+                    data = data,
+                    lng = ~LONG,
+                    lat = ~LAT,
+                    radius = 6,
+                    weight = 2,
+                    fillColor = ~ pal(WQ_Plot_Colour),
+                    color = "black",
+                    stroke = TRUE,
+                    opacity = 0.5,
+                    fill = TRUE,
+                    fillOpacity = 1,
+                    group = "points",
+                    popup = popupGraph(
+                        plots,
+                        width = popup_width,
+                        height = popup_height
+                    )
+                )
+        })
+    }
+    # Clear existing points before adding new ones
+    mapProxy |> clearGroup("points")
+
+    # Generate ggplot
+    ggplot(WQ_plot_data, aes(x = survey_date, y = value)) +
+        geom_point(color = site_data$WQ_Plot_Colour) +
+        geom_line(color = site_data$WQ_Plot_Colour) +
+        labs(
+            title = paste("Water Chemistry -", reading_type),
+            x = "Date",
+            y = "Value"
+        ) +
+        theme_minimal()
+}
