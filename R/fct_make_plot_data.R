ed31a2ee8209a0661a80a92841f09a21098d97a2
stawitcc <c.c.stawitz@bham.ac.uk>
2025-10-20 17:07:18 +0100

    start updating levels of water chemistry readings

--- R/fct_make_plot_data.R
+++ R/fct_make_plot_data.R
@@ -239,35 +239,12 @@ species_plots <- function(riverfly_data, sampling_locs) {
 make_water_quality_plot_data <- function(
     water_quality_data,
     sampling_locs,
-    reading_type
+    plot_palette
 ) {
-    # Select only the relevant reading type and rename
-    waterQuality_Map <- water_quality_data |>
-        select(
-            organisation,
-            survey_date,
-            sampling_site,
-            !!as.name(reading_type)
-        ) |>
-        rename(value = !!as.name(reading_type)) |>
-        mutate(value = as.numeric(value))
-
-    # Determine breaks for water quality data
-    # Based on 95% quantile
-    break_endpoint <- quantile(
-        waterQuality_Map$value,
-        c(0.025, .975),
-        na.rm = TRUE
-    )
-    wq_breaks <- c(
-        -Inf,
-        seq(break_endpoint[1], break_endpoint[2], length.out = 8),
-        Inf
-    )
-
-    # Placeholder function for future water quality plot data processing
+    browser()
+    # Get locations of water quality observations
     water_quality_plots <- left_join(
-        waterQuality_Map,
+        water_quality_data,
         sampling_locs[, c(
             "sampling_site",
             "LAT",
@@ -280,11 +257,77 @@ make_water_quality_plot_data <- function(
         anonymise_organisations() |>
         remove_parenthesised_orgs() |>
         mutate(across(sampling_site, flip_site_names)) |>
+        mutate(ammonia_ppm = as.numeric(ammonia_ppm))
+
+    water_quality_plots <- water_quality_plots |>
+        dplyr::select(
+            organisation,
+            survey_date,
+            LONG,
+            LAT,
+            sampling_site,
+            one_of(unlist(water_quality_bw))
+        ) |>
+        tidyr::pivot_longer(
+            cols = -c(organisation, survey_date, sampling_site, LONG, LAT)
+        ) |>
+        dplyr::rename(reading_type = "name", value = "value") |>
+        mutate(value = as.numeric(value)) |>
         mutate(
-            WQ_Plot_Colour = cut(
-                value,
-                breaks = wq_breaks,
-                labels = brewer.pal(n = 9, name = "Blues")
+            ####Need to better format these first three in terms of ranges, nice colours etc
+            WQ_Plot_Colour = case_when(
+                reading_type == "conductivity_mS" & value <= 350 ~
+                    plot_palette[1],
+                reading_type == "conductivity_mS" & value > 350 & value < 450 ~
+                    plot_palette[2],
+                reading_type == "conductivity_mS" & value >= 450 & value < 550 ~
+                    plot_palette[3],
+                reading_type == "conductivity_mS" & value >= 550 & value < 650 ~
+                    plot_palette[4],
+                reading_type == "conductivity_mS" & value >= 650 ~
+                    plot_palette[5],
+                reading_type == "temperature_C" & value < 5 ~ plot_palette[1],
+                reading_type == "temperature_C" & value >= 5 & value < 10 ~
+                    plot_palette[2],
+                reading_type == "temperature_C" & value >= 10 & value < 20 ~
+                    plot_palette[3],
+                reading_type == "temperature_C" & value >= 20 & value < 25 ~
+                    plot_palette[4],
+                reading_type == "temperature_C" & value >= 25 ~ plot_palette[5],
+                reading_type == "ammonia_ppm" & value < 0.05 ~ plot_palette[1],
+                reading_type == "ammonia_ppm" & value >= 0.05 & value < 0.15 ~
+                    plot_palette[2],
+                reading_type == "ammonia_ppm" & value >= 0.15 & value < .25 ~
+                    plot_palette[3],
+                reading_type == "ammonia_ppm" & value >= 0.25 & value < .58 ~
+                    plot_palette[4],
+                reading_type == "ammonia_ppm" & value >= 0.58 ~ plot_palette[5],
+                reading_type == "phosphate_ppm" & value <= 0.05 ~
+                    plot_palette[1],
+                reading_type == "phosphate_ppm" &
+                    (value >= 0.05 & value < 0.1) ~
+                    plot_palette[2],
+                reading_type == "phosphate_ppm" & (value >= 0.2 & value < 0.5) ~
+                    plot_palette[3],
+                reading_type == "phosphate_ppm" & (value >= 0.5 & value <= 1) ~
+                    plot_palette[4],
+                reading_type == "phosphate_ppm" & value > 1 ~ plot_palette[5],
+                reading_type == "nitrate_ppm" & value <= 0.5 ~ plot_palette[1],
+                reading_type == "nitrate_ppm" & (value > 0.5 & value <= 1) ~
+                    plot_palette[2],
+                reading_type == "nitrate_ppm" & value > 1 & value <= 2 ~
+                    plot_palette[3],
+                reading_type == "nitrate_ppm" & (value > 2 & value <= 5) ~
+                    plot_palette[4],
+                reading_type == "nitrate_ppm" & value > 5 ~ plot_palette[5],
+                reading_type == "turbidity_NTU" & value < 17 ~ plot_palette[1],
+                reading_type == "turbidity_NTU" & value >= 17 & value < 25 ~
+                    plot_palette[2],
+                reading_type == "turbidity_NTU" & value >= 25 & value < 40 ~
+                    plot_palette[3],
+                reading_type == "turbidity_NTU" & value >= 40 & value < 100 ~
+                    plot_palette[4],
+                reading_type == "turbidity_NTU" & value >= 100 ~ plot_palette[5]
             )
         ) |>
         dplyr::mutate(survey_date = dmy(survey_date))
