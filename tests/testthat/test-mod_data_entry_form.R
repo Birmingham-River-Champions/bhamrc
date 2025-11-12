cd64fddc3ca24c4f166ebe348936ea87ea5cb20b
stawitcc <c.c.stawitz@bham.ac.uk>
2025-11-12 17:28:40 +0000

    add form test

---
+++ tests/testthat/test-mod_data_entry_form.R
@@ -0,0 +1,13 @@
+library(testthat)
+library(shiny)
+
+test_that("UI contains expected uiOutput id", {
+    ui <- mod_data_entry_form_ui("myid")
+    tags <- shiny::renderTags(ui)
+    html <- tags$html
+    expect_true(grepl("myid-form_ui", html))
+})
+
+test_that("server function exists", {
+    expect_true(is.function(mod_data_entry_form_server))
+})
