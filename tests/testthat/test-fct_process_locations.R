ffcf41d6f1bd26e4f702e3c6d59811b8af6b2485
stawitcc <c.c.stawitz@bham.ac.uk>
2025-11-12 13:34:11 +0000

    add tests

--- tests/testthat/test-fct_process_locations.R
+++ tests/testthat/test-fct_process_locations.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("process_locations works", {
+  test_df <- test_fixture_riverfly()
+  test_locs <- test_df$riverfly_locs
+
+  expect_equal(
+    process_locations(test_df$riverfly_locs),
+    test_locs
+  )
 })
