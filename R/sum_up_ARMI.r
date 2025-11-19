77f0f4a0634f5da694dc2a1c8af13dd7280ef512
Christine Stawitz <christine.corlett@gmail.com>
2025-11-19 09:34:20 +0000

    Update R/sum_up_ARMI.r
    Co-authored-by: garciaay <100789815+garciaay@users.noreply.github.com>

--- R/sum_up_ARMI.r
+++ R/sum_up_ARMI.r
@@ -15,7 +15,7 @@ sum_up_ARMI <- function(Riverfly_ARMI) {
                         na.rm = TRUE
                     )
                 ) |> #,
-                ##Ntaxa = sum(c_across(matches("Number of")) > 0, na.rm = TRUE))|> THOUGHT IT WAS CALCULATED LIKE ASPT, BUT IS ACTUALLY LIKE BMWP
+                ##Ntaxa = sum(c_across(matches("Number of")) > 0, na.rm = TRUE))|> Calculated like bmwp (not aspt)
                 dplyr::select(-all_of(names(riverfly_spp_bw)))
         ) # Should replace this with name constant eventually)
     return(Riverfly_ARMI_Calc)
