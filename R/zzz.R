b322ad47b1835741100c1f654100727b1fefc1f2
stawitcc <c.c.stawitz@bham.ac.uk>
2025-12-09 20:50:08 +0000

    try deauth only for non interactive sessions

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,8 @@
     options(bhamrc.dbname = "data.sqlite")
 
     # Initialize and populate db when package is loaded
-    googlesheets4::gs4_deauth()
+    if (!rlang::is_interactive()) {
+        googlesheets4::gs4_deauth()
+    }
     turn_gsheets_into_db()
 }
