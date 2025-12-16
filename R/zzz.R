e8560949b670f34e6664884ed38801294e7b734f
stawitcc <c.c.stawitz@bham.ac.uk>
2025-12-16 13:53:32 +0000

    replace with new token

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
 
     # Initialize and populate db when package is loaded
     googlesheets4::gs4_auth(
-        path = ".secrets/birminghamriverchampions-1b4a4b469009.json"
+        path = "./inst/extdata/birminghamriverchampions-433995c2fddf.json"
     )
     turn_gsheets_into_db()
 }
