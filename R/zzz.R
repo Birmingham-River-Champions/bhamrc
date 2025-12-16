187a7c804ed7131aea36e15451173ed284f42a2d
stawitcc <c.c.stawitz@bham.ac.uk>
2025-12-16 16:56:46 +0000

    deauth

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,9 @@
     options(bhamrc.dbname = "data.sqlite")
 
     # Initialize and populate db when package is loaded
-    googlesheets4::gs4_auth(
-        path = "./inst/extdata/birminghamriverchampions-433995c2fddf.json"
-    )
+    #googlesheets4::gs4_auth(
+    #    path = "./inst/extdata/birminghamriverchampions-433995c2fddf.json"
+    #)
+    googlesheets4::gs4_deauth()
     turn_gsheets_into_db()
 }
