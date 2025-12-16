39e89709ad761dff44f9afac761e22361bfca192
stawitcc <c.c.stawitz@bham.ac.uk>
2025-12-16 14:08:56 +0000

    add if stattement to allow for GHA

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,12 @@
     options(bhamrc.dbname = "data.sqlite")
 
     # Initialize and populate db when package is loaded
-    googlesheets4::gs4_auth(
-        path = "./inst/extdata/birminghamriverchampions-433995c2fddf.json"
-    )
+    if (Sys.info()[["user"]] %in% c("stawitcc", "whitejcz")) {
+        googlesheets4::gs4_auth(
+            path = "./inst/extdata/birminghamriverchampions-433995c2fddf.json"
+        )
+    } else {
+        googlesheets4::gs_deauth()
+    }
     turn_gsheets_into_db()
 }
