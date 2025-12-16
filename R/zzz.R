8df28a14ca4936640de1e7e051b642b18c99b50e
Christine Stawitz <christine.corlett@gmail.com>
2025-12-16 14:34:00 +0000

    replace with new token (#37)
    * replace with new token
    
    * add if stattement to allow for GHA
    
    * import needed functions from googlesheets4
    
    * fix function call
    
    ---------
    
    Co-authored-by: stawitcc <c.c.stawitz@bham.ac.uk>

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,14 @@
+#' @importFrom googlesheets4 gs4_auth gs4_deauth
 .onLoad <- function(libname, pkgname) {
     options(bhamrc.dbname = "data.sqlite")
 
     # Initialize and populate db when package is loaded
-    googlesheets4::gs4_auth(
-        path = ".secrets/birminghamriverchampions-1b4a4b469009.json"
-    )
+    if (Sys.info()[["user"]] %in% c("stawitcc", "whitejcz")) {
+        googlesheets4::gs4_auth(
+            path = "./inst/extdata/birminghamriverchampions-433995c2fddf.json"
+        )
+    } else {
+        googlesheets4::gs4_deauth()
+    }
     turn_gsheets_into_db()
 }
