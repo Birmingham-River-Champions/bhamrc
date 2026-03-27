5afb563f330a460362f93cc6064e263be9e2d0a2
stawitcc <c.c.stawitz@bham.ac.uk>
2026-03-27 10:46:32 +0000

    Revert "try to get oauth working for gmailr"
    This reverts commit e227ddc0b895804a0be46f7f1f8dd34c978ebe08.

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,9 @@
     options(bhamrc.dbname = "data.sqlite")
 
     # Initialize and populate db when package is loaded
-    gmailr::gm_auth_configure(path = system.file("extdata","client_secret_764772056864-3am8o67h8h9dr1p8t1429ek6nfjccopc.apps.googleusercontent.com.json"))
-                      #system.file("extdata","birminghamriverchampions-f263e313b398.json"))
+    #googlesheets4::gs4_auth(
+    #    path = "./inst/extdata/birminghamriverchampions-36c3c5598be5.json"
+    #)
     googlesheets4::gs4_deauth()
     turn_gsheets_into_db()
 }
