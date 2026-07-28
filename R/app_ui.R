5143daa5d03e082aca9774707aca1cdc1df46b2a
James Tripp <j.tripp.1@bham.ac.uk>
2026-07-28 16:17:56 +0100

    Test printout to verify back end data shown

--- R/app_ui.R
+++ R/app_ui.R
@@ -40,7 +40,8 @@ app_ui <- function(request) {
                 "<div>Monitor <span>your</span> river</div>
                 <div>Protect <span>your</span> river</div>
                 <div>Love <span>your</span> river</div>"
-              )
+              ),
+              verbatimTextOutput("be_status")
             )
           ),
           # Right side: Smaller footer images, with EA logo slightly bigger
