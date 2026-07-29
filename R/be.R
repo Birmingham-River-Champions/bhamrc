eb18b96143c796a2ddb0303878ced6f66cd44f8c
James Tripp <j.tripp.1@bham.ac.uk>
2026-07-29 14:54:05 +0100

    Moved all be related functions into single file

--- R/be.R
+++ R/be.R
@@ -1,3 +1,17 @@
+# Creates environment to store back end variables
+# Runs before Shiny process starts so environment is accessible
+# to all subsequently created Shiny sessions
+be <- new.env(parent = emptyenv())
+
+be$running <- FALSE
+be$job <- NULL
+be$timestamp <- "Never"
+be$data <- NULL
+be$run <- 0
+
+be$started <- FALSE
+
+
 #' Starts an asynchonous job in the background
 #' @importFrom mirai mirai
 #' @importFrom googlesheets4 gs4_auth read_sheet
@@ -108,4 +122,81 @@ be_start <- function() {
       )
     })
   }
+}
+
+# R/be_startup.R
+be_startup <- function() {
+  mirai::daemons(1)
+  # Start polling for completed jobs
+  be_poll()
+
+  # Trigger initial refresh immediately
+  be_start()
+
+  # Schedule future refreshes
+  be_schedule_check(
+    interval = 1
+  )
+}
+
+#' Triggers check of backend job every second
+#' @importFrom later later
+#' @noRd
+be_poll <- function() {
+  be_check()
+
+  later(
+    be_poll,
+    delay = 0.1
+  )
+}
+
+# Checks if backend job running.
+# Once complete moves data to be$data and sets flags
+#' @importFrom mirai unresolved
+#' @noRd
+be_check <- function() {
+  # Just return if job is null or unresoved
+  # see https://mirai.r-lib.org/reference/unresolved.html
+  if (is.null(be$job)) {
+    return()
+  }
+
+  if (unresolved(be$job)) {
+    return()
+  }
+
+  # Below code only runs when the mirai job is
+  # neither unresolved or not running.
+
+  # pass result to be$data for convenience
+  be$timestamp <- be$job$timestamp # Record when run
+
+  be$run <- be$run + 1 #iterate run count
+
+  # Update data value when marai back end job
+  # contains data
+  if (!is.null(be$job$data)) {
+    be$data <- be$job$data
+  }
+
+  # Clear mira back end job data and set job running flag to false
+  be$job <- NULL
+  be$running <- FALSE
+}
+
+# Checks if back end job running.
+# Once complete moves data to be$data an sets flags
+#' @param interval interval between checks
+#' @importFrom later later
+#' @noRd
+be_schedule_check <- function(interval = be_interval) {
+  be_start()
+
+  later(
+    function() {
+      be_schedule_check(interval)
+    },
+    delay = interval
+  )
 }
