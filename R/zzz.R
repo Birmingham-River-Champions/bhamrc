693d3e74c8dd805e367a6eb520106738740c9111
stawitcc <c.c.stawitz@bham.ac.uk>
2026-07-30 19:03:08 +0100

    Revert "update path to auth"
    This reverts commit 90a6f942fffee9e102f40d41ea00ed9058c8e5f2.

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,9 @@
     options(bhamrc.dbname = "data.sqlite")
 
     # Initialize and populate db when package is loaded
-    googlesheets4::gs4_auth(
-        path = "./inst/extdata/birminghamriverchampions-36c3c5598be5.json"
-    )
+    #googlesheets4::gs4_auth(
+    #    path = "./inst/extdata/birminghamriverchampions-36c3c5598be5.json"
+    #)
+    googlesheets4::gs4_deauth()
     turn_newsheet_into_db()
 }
