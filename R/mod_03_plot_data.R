434100df63706050d458d8bdd304957755d41774
James Tripp <j.tripp.1@bham.ac.uk>
2026-08-06 18:22:37 +0100

    Removed older code and dependency on load_data

--- R/mod_03_plot_data.R
+++ R/mod_03_plot_data.R
@@ -123,10 +123,6 @@ mod_03_plot_data_ui <- function(id) {
 mod_03_plot_data_server <- function(id, be_result) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    # Load the data and assign to variables in the module environment
-    data_plot_list <- load_data()
-    list2env(data_plot_list, envir = environment())
-
     # Initialize the Leaflet map
     output$submission_map <- renderLeaflet({
       leaflet(
@@ -196,119 +192,6 @@ mod_03_plot_data_server <- function(id, be_result) {
     selected_invasive_type <- reactive(input$invasiveType)
     selected_reading_type <- reactive(input$readingType)
     screen_width <- reactive(input$screen_width)
-
-    # # Update the map with appropriate data
-    # updateMap <- function(input, output, session) {
-    #   mapProxy <- leafletProxy("map")
-
-    #   mapProxy |>
-    #     clearMapLayers()
-
-    #   if (selected_metric() == "Urban Riverfly") {
-    #     if (selected_riverfly() == "ARMI") {
-    #       mapProxy <- leafletProxy("map") |>
-    #         addARMIMarkers(
-    #           map_data = riverflyARMIMap,
-    #           popup_data = Riverfly_ARMI_Popups,
-    #           screen_width = screen_width()
-    #         ) |>
-    #         showGroup("ARMI points")
-    #     } else if (selected_riverfly() == "Urban Riverfly species") {
-    #       # If the user chooses Urban Riverfly species, plot abundance data
-    #       # Filter by the selected Taxa
-    #       selectedTaxa <- names(which(
-    #         riverfly_spp_bw == selected_riverfly_species()
-    #       ))
-    #       riverfly_species_popups <- Riverfly_Species_Plot[grepl(
-    #         selectedTaxa,
-    #         names(Riverfly_Species_Plot)
-    #       )]
-    #       mapProxy |>
-    #         addRiverflySpeciesMarkers(
-    #           popup_data = riverfly_species_popups,
-    #           map_data = Riverfly_Species_Plot_Recent[[selectedTaxa]],
-    #           selectedTaxa,
-    #           screen_width()
-    #         ) |>
-    #         showGroup("Riverfly points")
-    #     } else if (selected_riverfly() == "Other species") {
-    #       # If the user chooses Other species, plot abundance data
-    #       # Filter data for the selected 'other species' from the radio buttons
-    #       selectedTaxa <- names(which(
-    #         other_spp_bw == selected_other_species()
-    #       ))
-    #       otherspeciesData_Recent_Map <- Riverfly_Other_Species_Plot_Recent[[
-    #         selectedTaxa
-    #       ]]
-    #       mapProxy |>
-    #         addOtherSpeciesMarkers(
-    #           otherspeciesData_Recent_Map,
-    #           selectedTaxa
-    #         ) |>
-    #         showGroup("Other spp points")
-    #     }
-    #   } else if (selected_metric() == "Invasive Species") {
-    #     # If the user chooses Invasive Species, plot presence/absence data
-    #     mapProxy |>
-    #       addInvasiveSpeciesMarkers(
-    #         BRCInvSpcs_Plot_Recent,
-    #         selected_invasive_type(),
-    #         rev(brewer.pal(n = 4, name = "Blues"))
-    #       ) |>
-    #       showGroup("Invasive points")
-    #   } else if (selected_metric() == "Water Chemistry") {
-    #     # If the user chooses Water Chemistry, plot water quality data
-
-    #     wq_Recent_Map <- WQ_plot_data$recent[[selected_reading_type()]]
-
-    #     wq_data <- WQ_plot_data$all_obs[grepl(
-    #       selected_reading_type(),
-    #       names(WQ_plot_data$all_obs)
-    #     )]
-
-    #     mapProxy |>
-    #       addWaterQualityMarkers(
-    #         wq_data = wq_data,
-    #         wq_data_recent = wq_Recent_Map,
-    #         metric = selected_reading_type(),
-    #         screen_width = screen_width()
-    #       )
-
-    #     mapProxy |>
-    #       showGroup("Water Quality points")
-    #   }
-    #   # mapProxy |>
-    #   #   addLayersControl(
-    #   #     overlayGroups = c(
-    #   #       "Riverfly points",
-    #   #       "Invasive points",
-    #   #       "Other spp points",
-    #   #       "ARMI points",
-    #   #       "Water Quality points"
-    #   #     ),
-    #   #     options = layersControlOptions(
-    #   #       collapsed = FALSE,
-    #   #       position = "topright"
-    #   #     )
-    #   #   )
-    # }
-
-    # observeEvent(
-    #   {
-    #     list(
-    #       input$metric,
-    #       input$readingType,
-    #       input$invasiveType,
-    #       input$riverfly,
-    #       input$riverflySpecies,
-    #       input$otherSpecies
-    #     )
-    #   },
-    #   {
-    #     updateMap(input, output, session)
-    #   },
-    #   ignoreInit = TRUE
-    # )
   })
 }
 
