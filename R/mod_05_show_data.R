1eb490d0b277177813c3f3df64d681a0ccf4ca08
James Tripp <j.tripp.1@bham.ac.uk>
2026-08-14 08:59:04 +0100

    Data table improvements

--- R/mod_05_show_data.R
+++ R/mod_05_show_data.R
@@ -12,11 +12,43 @@ mod_05_show_data_ui <- function(id) {
   ns <- NS(id)
   sidebarLayout(
     sidebarPanel(
-      data_type_input_ui(ns("data_type"), which_data_types = c(1, 2)) # To add more data types, change the vector here (add 3 for invasive species, add 5 for outfall safari)
+      width = 3,
+      data_type_input_ui(ns("data_type"), which_data_types = c(1, 2)), # To add more data types, change the vector here (add 3 for invasive species, add 5 for outfall safari)
+      # Search and download buttons displayed in left column
+      # Search function uses the datatable native functionality
+      textInput(ns("table_search"), "Search"),
+      downloadButton(ns("download_csv"), "Download CSV"),
+      downloadButton(ns("download_xlsx"), "Download XLSX")
     ),
     mainPanel(
-      DTOutput(ns("dt_submissions")),
-      #textOutput(ns("survey")),
+      width = 9,
+      # The below styling does the following within hide-dt-search div:
+      #.     Hides the search box displayed above the datatable
+      #.     Sets margin, padding and min height to 0
+      #.     The table headers do not wrap so all column headers are one
+      #.         line. This means the table height is consistent between datasets.
+      tags$style(HTML(
+        "
+    .hide-dt-search .dataTables_filter {
+      display: none !important;
+    }
+    .hide-dt-search .dataTables_wrapper > .row:first-child {
+      margin: 0 !important;
+      padding: 0 !important;
+      min-height: 0 !important;
+    }
+    .hide-dt-search table.dataTable th {
+      white-space: nowrap;
+    }
+  "
+      )),
+      # Put submission table into hide-dt-search dive
+      # so the formatting rules above apply to the datatable
+      div(
+        class = "hide-dt-search",
+        DTOutput(ns("dt_submissions"))
+      ),
+      textOutput(ns("survey")),
       #textOutput(ns("table_name")),
       #downloadButton(ns("download_data"), "Download Data"),
       #DT::DTOutput(ns("entries"))
@@ -28,7 +60,8 @@ mod_05_show_data_ui <- function(id) {
 #' @importFrom DT renderDT DTOutput
 #' @importFrom writexl write_xlsx
 #' @importFrom stats setNames
-#' @importFrom dplyr select
+#' @importFrom dplyr select rename_with
+#' @importFrom stringr str_replace_all str_to_sentence
 #' @noRd
 mod_05_show_data_server <- function(id, be_result) {
   moduleServer(id, function(input, output, session) {
@@ -64,49 +97,105 @@ mod_05_show_data_server <- function(id, be_result) {
     # of the data in one table, then (c) filter the
     # rows and columns of the table dynamically.
 
+    # Filters the data in one location
+    filtered_data <- reactive({
+      req(!is.null(be_result()), survey())
+      be_result() |> filter(dataset == survey_map[[survey()]])
+    })
+
     # Our initial table needs to contain _all_ of the
     # possible columns. The dataframe is created in the
     # file
     # Create the proxy
     dt_proxy <- dataTableProxy("dt_submissions")
-    output$dt_submissions <- renderDataTable({
-      # Show data if available
+    output$dt_submissions <- DT::renderDataTable({
+      # Show data if available, otherwise blank table
       initial_data <- isolate({
         if (
           !is.null(be_result()) && nrow(be_result()) > 0 && !is.null(survey())
         ) {
-          be_result() |> filter(dataset == survey_map[[survey()]])
+          filtered_data() |>
+            select(!c("Organisation")) |>
+            rename_with(~ str_replace_all(.x, "_", " ") |> str_to_sentence())
         } else {
-          create_blank_submission_df()
+          create_blank_submission_df() |>
+            select(!c("Organisation")) |>
+            rename_with(~ str_replace_all(.x, "_", " ") |> str_to_sentence())
         }
       })
 
+      # Configure data table. Search
+      # pagination, n out of x text at top of table.
+      # X and Y scrolling enabled.
       datatable(
         initial_data,
-        extensions = "Buttons",
+        width = "100%",
         options = list(
-          dom = "Bfrtip",
-          buttons = list(
-            list(
-              extend = "csv",
-              text = '<i class="fa fa-download"></i> Download CSV',
-              filename = "birmingham_river_champions_submissions",
-              exportOptions = list(
-                columns = ":visible"
-              )
-            ),
-            list(
-              extend = "excel",
-              text = '<i class="fa fa-download"></i> Download XLSX',
-              filename = "birmingham_river_champions_submissions",
-              exportOptions = list(
-                columns = ":visible"
-              )
-            )
-          )
+          dom = "fript",
+          scrollX = TRUE,
+          scrollY = "60vh",
+          scrollCollapse = TRUE,
+          autoWidth = TRUE
         )
       )
     })
+
+    excluded_cols <- c(
+      "Organisation",
+      "long",
+      "lat",
+      "email_address",
+      "survey_date"
+    )
+
+    # Data prepared for download
+    # Data is filtered to show a subset of columns,
+    #   and based on search terms in the search box.
+    download_data <- reactive({
+      # Only trigger if data is available and survey selected
+      req(!is.null(be_result()), survey())
+
+      # Get columns for specific dataset and only show those columns
+      cols_to_show <- get_relevant_dataset_columns(survey_map[[survey()]])
+      cols_to_show <- setdiff(cols_to_show, excluded_cols)
+
+      # Filter dataframe to only show those columns
+      data <- filtered_data() |>
+        select(any_of(cols_to_show))
+
+      # Get search string
+      search_term <- input$table_search
+
+      # If search_term is not empty
+      if (!is.null(search_term) && nzchar(search_term)) {
+        # Find rows containing search term
+        match_rows <- apply(data, 1, function(row) {
+          any(grepl(search_term, row, ignore.case = TRUE, fixed = FALSE))
+        })
+
+        # Get these matching rows
+        data <- data[match_rows, , drop = FALSE]
+      }
+
+      # Return filtered data
+      return(
+        data
+      )
+    })
+
+    # Download handlers which use above filtered download_data()
+    output$download_csv <- downloadHandler(
+      filename = function() "birmingham_river_champions_submissions.csv",
+      content = function(file) {
+        write.csv(download_data(), file, row.names = FALSE)
+      }
+    )
+    output$download_xlsx <- downloadHandler(
+      filename = function() "birmingham_river_champions_submissions.xlsx",
+      content = function(file) {
+        writexl::write_xlsx(download_data(), file)
+      }
+    )
 
     # Update data based on drop down selection
     survey_map <- c(
@@ -118,25 +207,36 @@ mod_05_show_data_server <- function(id, be_result) {
 
     # Update table when either be_result or survey
     # selection changes.
+    # Note: we use DT:: to use the datatable updated functions
     observeEvent(
       list(be_result(), survey()),
       {
         req(!is.null(be_result()))
         req(survey())
-        replaceData(
+        DT::replaceData(
           dt_proxy,
-          be_result() |>
-            filter(dataset == survey_map[[survey()]]),
+          filtered_data() |>
+            select(!c("Organisation")) |>
+            # Clear up column names
+            rename_with(~ str_replace_all(.x, "_", " ") |> str_to_sentence()),
           resetPaging = FALSE,
           clearSelection = "none",
         )
 
         # filter out the data not relevent to
         # the dataset we're looking at
-        all_cols <- names(be_result())
+        pretty_cols <- function(x) {
+          x |>
+            setdiff(c("Organisation", "long", "lat")) |>
+            str_replace_all("_", " ") |>
+            str_to_sentence()
+        }
 
-        cols_to_show <- get_relevant_dataset_columns(survey_map[[survey()]])
+        all_cols <- names(be_result()) |>
+          pretty_cols()
 
+        cols_to_show <- get_relevant_dataset_columns(survey_map[[survey()]]) |>
+          pretty_cols()
         show_cols <- which(all_cols %in% cols_to_show)
         hide_cols <- which(!all_cols %in% cols_to_show)
 
@@ -146,6 +246,11 @@ mod_05_show_data_server <- function(id, be_result) {
         hideCols(dt_proxy, hide_cols)
       }
     )
+
+    # Update search in datatable when table_search value changes
+    observeEvent(input$table_search, {
+      dt_proxy |> DT::updateSearch(keywords = list(global = input$table_search))
+    })
 
     # We want the table to be rendered
     # when the app starts. Rather than only when
