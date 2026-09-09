6ffd72873433cbc748c4670c7a8dcf3dad4af91f
James Tripp <j.tripp.1@bham.ac.uk>
2026-09-09 20:02:15 +0100

    Refer correctly to google cred file #60

--- R/zzz.R
+++ R/zzz.R
@@ -2,22 +2,15 @@
     options(bhamrc.dbname = "data.sqlite")
 
     # If running locally
-    # googlesheets4::gs4_auth(
-    #     path = "./inst/extdata/birminghamriverchampions-db5399f61d80.json"
-    # )
+    googlesheets4::gs4_auth(
+        path = "./inst/extdata/birminghamriverchampions-db5399f61d80.json"
+    )
 
     # If running on github repo
-    cred_file <- Sys.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
-    if (nzchar(cred_file) && file.exists(cred_file)) {
-        googlesheets4::gs4_auth(path = cred_file)
-    } else {
-        googlesheets4::gs4_deauth()
-    }
+    cred_file <- "inst/extdata/birminghamriverchampions-db5399f61d80.json"
 
     # Verify credential is created
     message("cred_file = ", cred_file)
-
     message("exists = ", file.exists(cred_file))
 
     turn_newsheet_into_db()
