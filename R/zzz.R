847b2e6a2174dd457f204357f24f6ce0f2045861
James Tripp <j.tripp.1@bham.ac.uk>
2026-09-10 08:31:19 +0100

    Added debug string for cred file #60

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,17 @@
 .onLoad <- function(libname, pkgname) {
     options(bhamrc.dbname = "data.sqlite")
 
+    # If running on github repo
+    cred_file <- "inst/extdata/birminghamriverchampions-db5399f61d80.json"
+
+    message("exists = ", file.exists(cred_file))
+    message("size = ", file.info(cred_file)$size)
+    cat(readLines(cred_file, n = 3), sep = "\n")
+
     # If running locally
     googlesheets4::gs4_auth(
         path = "./inst/extdata/birminghamriverchampions-db5399f61d80.json"
     )
-
-    # If running on github repo
-    cred_file <- "inst/extdata/birminghamriverchampions-db5399f61d80.json"
-
-    # Verify credential is created
-    message("cred_file = ", cred_file)
-    message("exists = ", file.exists(cred_file))
 
     turn_newsheet_into_db()
 }
