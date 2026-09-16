cdaa48759dbb3500085d8f87414d7f8afa463521
James Tripp <j.tripp.1@bham.ac.uk>
2026-09-16 16:06:42 +0100

    Replaced url and json location #60

--- R/config.R
+++ R/config.R
@@ -320,7 +320,14 @@ choices_list <- list(
 
 # Read data source URL
 # So we can parameterise this within github actions
-URL <- readLines("inst/extdata/google_url.txt", warn = FALSE)
+URL <- readLines(
+    system.file(
+        "extdata",
+        "google_url.txt",
+        package = "bhamrc"
+    ),
+    warn = FALSE
+)
 
 # This is the ID for the new sheet with four separate tables
 google_sheet_id <- URL
--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,11 @@
     options(bhamrc.dbname = "data.sqlite")
 
     googlesheets4::gs4_auth(
-        path = "./inst/extdata/birminghamriverchampions-db5399f61d80.json"
+        path = system.file(
+            "extdata",
+            "birminghamriverchampions-db5399f61d80.json",
+            package = "bhamrc"
+        )
     )
 
     turn_newsheet_into_db()
