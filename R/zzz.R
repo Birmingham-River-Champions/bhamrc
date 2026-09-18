202de7ae7ae2e7e961ac8d7160fbcd529a8f3c81
James Tripp <j.tripp.1@bham.ac.uk>
2026-09-18 16:45:47 +0100

    Allow system.file and relative path loading #60

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,21 @@
 .onLoad <- function(libname, pkgname) {
     options(bhamrc.dbname = "data.sqlite")
 
-    googlesheets4::gs4_auth(
-        path = "inst/extdata/birminghamriverchampions-db5399f61d80.json"
+    # Check if system.file reference is present
+    # if not then load from relative path
+    json_path <- system.file(
+        "extdata",
+        "birminghamriverchampions-db5399f61d80.json",
+        package = pkgname
     )
+
+    if (!nzchar(json_path)) {
+        json_path <- "inst/extdata/birminghamriverchampions-db5399f61d80.json"
+    }
+
+    if (file.exists(json_path)) {
+        googlesheets4::gs4_auth(json_path)
+    }
 
     turn_newsheet_into_db()
 }
